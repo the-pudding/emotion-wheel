3e078bf5812b0f038aaeb05c795af82c38e3a12c
Michelle McGhee <michmac202@gmail.com>
2022-10-28 12:02:21 -0700

    update github pages

---
+++ docs/_app/immutable/chunks/1-0704a6f5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-1362a0d0.js";export{t as component};
--- docs/_app/immutable/chunks/2-132ac23b.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-eb56717b.js";const e=!0;export{t as component,e as server};
