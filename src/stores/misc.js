e375d06b5614af9e9abf360e77e3cea7335c7ca6
Michelle McGhee <michmac202@gmail.com>
2022-10-28 12:02:03 -0700

    balloon sound

--- src/stores/misc.js
+++ src/stores/misc.js
@@ -4,12 +4,12 @@ export const entered = writable(false);
 export const userId = writable(undefined);
 export const scrollX = writable(0);
 
-// export const basicFeeling = writable(undefined);
-// export const words = writable([]);
-// export const colors = writable([]);
-
-export const basicFeeling = writable("ok");
-export const words = writable(["happy", "sad"]);
+export const basicFeeling = writable(undefined);
+export const words = writable([]);
 export const colors = writable([]);
+
+// export const basicFeeling = writable("ok");
+// export const words = writable(["happy", "sad"]);
+// export const colors = writable([]);
 
 export const selectedGalleryImage = writable(undefined);
