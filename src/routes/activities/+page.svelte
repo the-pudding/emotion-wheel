c2a12c04b173f2eb9a4cd62b95dd5a2d51399599
Michelle McGhee <michmac202@gmail.com>
2022-12-12 10:27:08 -0800

    activities page meta

--- src/routes/activities/+page.svelte
+++ src/routes/activities/+page.svelte
@@ -1,5 +1,29 @@
 <script>
+	import Meta from "$components/Meta.svelte";
+
 	import Activities from "$components/Activities.svelte";
+	import copy from "$data/copy.json";
+	import version from "$utils/version.js";
+
+	version();
+
+	const preloadFont = [
+		"https://pudding.cool/assets/fonts/tiempos/TiemposTextWeb-Regular.woff2",
+		"https://pudding.cool/assets/fonts/tiempos/TiemposTextWeb-Bold.woff2",
+		"https://pudding.cool/assets/fonts/national/National2Web-Regular.woff2",
+		"https://pudding.cool/assets/fonts/national/National2Web-Bold.woff2"
+	];
+
+	const { activitiesTitle, activitiesDescription, activitiesUrl, keywords } =
+		copy;
 </script>
+
+<Meta
+	title={activitiesTitle}
+	description={activitiesDescription}
+	url={activitiesUrl}
+	{preloadFont}
+	{keywords}
+/>
 
 <Activities />
