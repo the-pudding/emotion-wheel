02af4f8a57340f494de0d59da45b9dcb73fcc53b
Michelle McGhee <michmac202@gmail.com>
2022-12-13 10:21:10 -0800

    facebook instagram in app browser warning

--- src/components/Rotate.svelte
+++ src/components/Rotate.svelte
@@ -1,7 +1,35 @@
 <script>
 	import wordmark from "$svg/wordmark.svg";
+	import Icon from "$components/helpers/Icon.svelte";
+	import { onMount } from "svelte";
 
 	export let visible;
+
+	let facebookOrInstagram = false;
+	let showCopied = false;
+
+	const copyToClipboard = () => {
+		navigator.clipboard.writeText(
+			"https://pudding.cool/2022/12/emotion-wheel/"
+		);
+		showCopied = true;
+
+		setTimeout(() => {
+			showCopied = false;
+		}, 2000);
+	};
+
+	const isFbOrIg = (str) => {
+		return (
+			str.includes("FBAN") ||
+			str.includes("FBAV") ||
+			str.toLowerCase().includes("instagram")
+		);
+	};
+
+	onMount(() => {
+		facebookOrInstagram = isFbOrIg(window.navigator.userAgent);
+	});
 </script>
 
 <div class="rotate" class:visible>
@@ -21,6 +49,17 @@
 	<h3>Please rotate your device.</h3>
 
 	<p>This immersive experience is designed to be viewed in landscape mode.</p>
+
+	{#if facebookOrInstagram}
+		<p class="fb" on:click={copyToClipboard}>
+			<Icon name="alert-triangle" />
+			Facebook and Instagram don't allow you to rotate your screen in-app. Click
+			here to copy the link, then go open it in your regular browser (i.e. Safari
+			or Chrome).
+		</p>
+
+		<p class="copied" class:visible={showCopied}>Link copied!</p>
+	{/if}
 </div>
 
 <style>
@@ -40,6 +79,28 @@
 	}
 	:global(.wordmark svg path) {
 		fill: currentColor;
+	}
+	.fb {
+		font-size: var(--16px);
+		position: absolute;
+		bottom: -1em;
+		background: white;
+		color: black;
+		padding: 1em;
+	}
+	.copied {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		background: white;
+		color: black;
+		padding: 0.6em;
+		opacity: 0;
+		transition: opacity calc(var(--1s) * 0.5);
+	}
+	.copied.visible {
+		opacity: 1;
 	}
 
 	.rotate {
